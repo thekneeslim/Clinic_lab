08839463668c8bce3ec77be2f1751527da2b727b
Denise Lim <denise.lim.sh@gmail.com>
2016-10-05 19:03:50 +0800

    changed column titles and foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005082924) do
+ActiveRecord::Schema.define(version: 20161005110222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,16 +24,20 @@ ActiveRecord::Schema.define(version: 20161005082924) do
   create_table "appointments", force: :cascade do |t|
     t.integer  "patient_id"
     t.integer  "doctor_id"
-    t.integer  "date_id"
-    t.integer  "time_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.integer  "appointment_date_id"
+    t.integer  "time_table_id"
+    t.index ["appointment_date_id"], name: "index_appointments_on_appointment_date_id", using: :btree
+    t.index ["time_table_id"], name: "index_appointments_on_time_table_id", using: :btree
   end
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "specialty_id"
+    t.index ["specialty_id"], name: "index_doctors_on_specialty_id", using: :btree
   end
 
   create_table "patients", force: :cascade do |t|
@@ -45,4 +49,19 @@ ActiveRecord::Schema.define(version: 20161005082924) do
     t.datetime "updated_at",      null: false
   end
 
+  create_table "specialties", force: :cascade do |t|
+    t.string   "specialty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "time_tables", force: :cascade do |t|
+    t.string   "time_table"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "appointments", "appointment_dates"
+  add_foreign_key "appointments", "time_tables"
+  add_foreign_key "doctors", "specialties"
 end
